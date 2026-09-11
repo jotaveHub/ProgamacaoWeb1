3dd77e2f078cfa0b0b960e20ed22fc98ccb2bb7a
jotaveHub <accurategx@gmail.com>
2026-09-11 17:30:45 -0300

    feat: adicionando negociação de conteúdo através do MediaType.

--- demo/src/main/java/br/edu/ifpb/demo/AlunosController.java
+++ demo/src/main/java/br/edu/ifpb/demo/AlunosController.java
@@ -1,24 +1,29 @@
 package br.edu.ifpb.demo;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunosController {
 
-    @GetMapping("/{matricula}")
+    @GetMapping(path = "/{matricula}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public String listar(
             @PathVariable String matricula,
             @RequestParam(required = false) String curso
     ) {
-        if (matricula.equals("123") && (curso == null || curso.equals("ADS"))) {
+        if(matricula.equals("123") &&
+                (curso == null || curso.equals("ADS"))){
             return "Aluno listado";
         }
         return "Aluno não encontrado";
     }
 
     @PostMapping
-    public String cadastrar() {
+    public String cadastrar(){
         return "Cadastrar Aluno";
     }
 }
